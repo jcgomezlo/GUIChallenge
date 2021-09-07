fd6b5d7c7b7c4b1409a3e1dd4344d1924e48cc7c
jcgomezlo <juancamilo.gomez@endava.com>
2021-09-07 14:13:42 -0500

    features

--- src/test/java/TestExample.java
+++ src/test/java/TestExample.java
@@ -1,13 +1,41 @@
 import org.apache.logging.log4j.Logger;
 import org.apache.logging.log4j.LogManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
+
+import java.util.List;
 
 
 public class TestExample {
     private static final Logger Logger = LogManager.getLogger(TestExample.class);
 
-   @Test
-    public void test(){
-       Logger.info("This is info debuf");
-   }
+    @Test
+    public void firstTest() throws InterruptedException {
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.navigate().to("https://www.wikipedia.org/");
+
+        By searchBar = By.id("searchInput");
+        By listLocator = By.cssSelector(".suggestions-dropdown >a");
+
+        driver.findElement(searchBar).sendKeys("Endava");
+
+        WebDriverWait wait = new WebDriverWait(driver, 3);
+        wait.until(ExpectedConditions.numberOfElementsToBeMoreThan(listLocator,0));
+
+        List<WebElement> suggestedItems = driver.findElements(listLocator);
+        suggestedItems.get(0).click();
+
+
+
+//        driver.quit();
+    }
+
 }
