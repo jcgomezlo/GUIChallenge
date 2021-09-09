04688eec90cfab77f391764c33fb4b2efb1aeb89
jcgomezlo <juancamilo.gomez@endava.com>
2021-09-09 09:14:00 -0500

    Remove unused imports and commented code

--- src/test/java/FilterTest.java
+++ src/test/java/FilterTest.java
@@ -4,7 +4,6 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.openqa.selenium.WebDriver;
 import org.testng.annotations.Test;
-import pages.LandingPage;
 import pages.MovieListPage;
 import pages.MoviePage;
 import utils.DriverFactory;
--- src/test/java/LoginTest.java
+++ src/test/java/LoginTest.java
@@ -4,7 +4,6 @@ import io.qameta.allure.Feature;
 import org.apache.logging.log4j.Logger;
 import org.apache.logging.log4j.LogManager;
 import org.openqa.selenium.WebDriver;
-import org.testng.Assert;
 import org.testng.annotations.Test;
 import pages.LandingPage;
 import pages.LoginPage;
@@ -30,20 +29,20 @@ public class LoginTest extends LoginDataProvider {
         assertThat(userPage.getUserTitle(),equalTo(username));
     }
 
-//    @Feature("Login")
-//    @Test(dataProvider = "InvalidCredentials")
-//    public void FailedLogin(String username, String password){
-//        logger.info("Failed Login test initiated ..");
-//        logger.info("Username: " + username);
-//        logger.info("Password: " + password);
-//
-//        WebDriver driver = DriverFactory.getInstance().getDriver();
-//        LandingPage landingPage = new LandingPage(driver);
-//        LoginPage loginPage = landingPage.goToLogin();
-//        loginPage.login(username,password);
-//        assertThat(loginPage.getErrorColor(),equalTo(LoginUtils.RGB_RED));
-//        assertThat(loginPage.getNumberOfErrorMessages(),equalTo(2));
-//    }
+    @Feature("Login")
+    @Test(dataProvider = "InvalidCredentials")
+    public void FailedLogin(String username, String password){
+        logger.info("Failed Login test initiated ..");
+        logger.info("Username: " + username);
+        logger.info("Password: " + password);
+
+        WebDriver driver = DriverFactory.getInstance().getDriver();
+        LandingPage landingPage = new LandingPage(driver);
+        LoginPage loginPage = landingPage.goToLogin();
+        loginPage.login(username,password);
+        assertThat(loginPage.getErrorColor(),equalTo(LoginUtils.RGB_RED));
+        assertThat(loginPage.getNumberOfErrorMessages(),equalTo(2));
+    }
 
 
 }
